65a339c7c8fdfad28db4fd9b1d3a8b6916358075
Dilshan Weerasekara <dilshan.weerasekara@hayleysadvantis.com>
2024-06-19 14:52:32 +0530

    initial commi

---
+++ jobdone_backend/Controllers/JobController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace jobdone_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpPost(Name = "SaveCompletedJob")]
+        public IEnumerable<Any> SaveCompletedJob()
+        {
+            return [];
+        }
+
+        [HttpGet(Name = "GetAllJobs")]
+        public IEnumerable<Any> GetAllJobs()
+        {
+            return [];
+        }
+
+        [HttpGet(Name = "GetAllJobsWithFilter")]
+        public IEnumerable<Any> GetAllJobsWithFilter()
+        {
+            return [];
+        }
+    }
+}
